84135071cf981d46da328339f51b278d9ee042a3
João Bernardo Dias de Brito <joaobernadobrito@dinfor.uniceub>
2026-03-26 21:23:45 -0300

    aula 26/03

---
+++ João Bernardo Dias de Brito/Aulas/vue_cli/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Meu App Vue</h1>
+    <p class="subtitulo">Meu subtitulo</p>
+    <br />
+    <home-component />
+    <br />
+    <usuario-component />
+    <br/>
+    <estilo-component/>
+  </div>
+</template>
+
+<script>
+import HomeComponent from "./components/HomeComponent.vue";
+import UsuarioComponent from "./components/UsuarioComponent.vue";
+import EstiloComponent from "./components/EstiloComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+  },
+  created(){
+    console.log("created");
+  },
+  mounted(){
+    console.log("mounted");
+  },
+  updated(){
+    console.log("updated");
+  },
+  errorCaptured(){
+    console.log("errorCaptured");
+  },
+
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+/*h1{
+  color: darkmagenta;
+  text-align: center;
+}
+
+.subtitulo{
+  color: darkseagreen;
+  font-family: 'Gill Sans';
+  text-align: center;
+}*/
+</style>
---
+++ João Bernardo Dias de Brito/Aulas/vue_cli/src/components/UsuarioComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <p>Nome: {{ nome }}</p>
+        <p>Email:{{ email }}</p>
+        <hr />
+        <h1 v-show="isBrasaCampeao">Brasil Campeão</h1>
+        <img :src="urlImg" width="300" height="200" />
+    </div>
+</template>
+
+<script>
+
+ export default{
+    name:"UsuarioComponent",
+    data(){
+        return{
+            nome:"João Bernardo",
+            email:"Bernardinho@gmail.com",
+            isBrasaCampeao: false,
+            urlImg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8CtDmloIwee-AXHP0OOFbpnWmyd5plbAwWA&s",
+        };
+    },
+    mounted(){
+       setTimeout(() => {
+        this.isBrasaCampeao = true;
+       }, 4 * 1000);
+
+       setTimeout(() => {
+        this.isBrasaCampeao = false; 
+        this.urlImg = "https://imagem.natelinha.uol.com.br/original/bbb21-juliette-chora_3121.jpeg";
+       }, 8 * 1000);
+
+    },
+ };
+
+</script>
+
+
+
+<style>
+
+</style>
